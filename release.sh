abfa5b6aa2cd5e840daf293f7e005277f48df9cf
apxxxxxxe <calcium629@gmail.com>
2022-05-29 23:02:00 +0900

    add: release.sh

---
+++ release.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+version="v$(grep version shioriUpdater.go | grep -oE '[0-9]\.[0-9]\.[0-9]')"
+if [ $? -eq 1 ]; then
+  echo "error: failed to detect the version" 1>&2
+  exit 1
+fi
+
+remote_latest_version=$(git describe --tags --abbrev=0)
+
+if [ ${version} == ${remote_latest_version} ]; then
+  echo "error: this version is the same as the latest remote one" 1>&2
+  exit 1
+fi
+
+GOOS=windows GOARCH=386 go build -o pkg/shioriupdater_windows_386.exe
+
+ghr "v${version}" pkg/
